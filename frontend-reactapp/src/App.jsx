12e8a0cfa0094a7066670bf4d1ceb6d826b3d39c
krishnachaitanya3105 <krishnachaitanya31052006@gmail.com>
2025-10-18 11:07:37 +0530

    updated

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
+
 
 function App() {
   return (
